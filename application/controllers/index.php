4aac63c29e8b2f2d32dca6d779c461c0c41f1daf
Sergio Ferrando <immawinnerbaby92@gmail.com>
2014-04-10 12:32:41 +0200

    Prueba usuario commit

---
+++ application/controllers/index.php
@@ -0,0 +1,39 @@
+<?php
+class index extends CI_Controller
+{
+	public function __construct()
+    {
+        parent::__construct();
+        //en esta zona cargaremos la cookie con los datos del usuario
+    }
+	function home()
+	{	
+		$this->load->database();//CARGAMOS LA BASE DE DATOS??PROBAR EL AUTOLOAD
+		$this->language ="spanish";
+		$this->lang->load('index', $this->language);
+		$this->load->model("Musuarios");
+		$user_log = $this->Musuarios->get_user_data(1);//le pasaremos la id del usuario registrado por la cookie
+		echo $this->lang->line("welcome_msg").$user_log[0]->nombre_completo."<br>";
+		//Saca los usernames de todos los usuarios
+		$users=$this->Musuarios->get_all_usernames();
+		
+		/*
+		foreach($users as $user)
+		{
+			echo $user->username;
+		} */
+
+		//Todos los datos de los usuarios
+		$allusers = $this->Musuarios->get_all_users_data();	
+		foreach($allusers as $data_users)
+		{
+			echo"<br>";
+			foreach($data_users as $data =>$valor)
+			{
+				echo $data.":".$valor."<pre>";
+			}
+			echo"<br>";
+		}
+	}
+}
+?>
