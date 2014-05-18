c31b9b6fb645b4bccccace62ab1815fac1d4d889
Sergio Ferrando <immawinnerbaby92@gmail.com>
2014-05-18 20:39:27 +0200

    more server mail

--- application/controllers/email.php
+++ application/controllers/email.php
@@ -4,20 +4,27 @@ class Email extends CI_Controller
 	public function __construct()
     {
         parent::__construct();
-       $this->load->library('email');
+        $this->load->library('email');
+          
     }
-	function index()
-	{	
-
-	    
-
+	function send_contact()
+	{
+        $config['protocol']    = 'smtp';
+        $config['smtp_host']    = 'ssl://smtp.gmail.com';
+        $config['smtp_port']    = '465';
+        $config['smtp_timeout'] = '7';
+        $config['smtp_user']    = 'wibuks.dev@gmail.com';
+        $config['smtp_pass']    = SMTP_PASS;
+        $config['charset']    = 'utf-8';
+        $config['newline']    = "\r\n";
+        $config['mailtype'] = 'text'; // or html
+        $config['validation'] = TRUE; // bool whether to validate email or not 
         $this->email->initialize($config);
 
         $this->email->from('yolo@gmail.com', 'YOLO');
         $this->email->to('sferrando.92@gmail.com'); 
         $this->email->subject('Email Test');
-        $this->email->message('Testing the email class.');  
-
+        $this->email->message('FUNSIONA NO MAS class.');  
         $this->email->send();
 
         echo $this->email->print_debugger();
